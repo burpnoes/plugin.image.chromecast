e6647d36a35ff6a6620e36055bb51ff4f96fa9a6
burpnoes <burpnoes@gmail.com>
2015-03-14 13:49:43 -0700

    Chromecast Kodi Image Plugin initial commit

--- default.py
+++ default.py
@@ -6,7 +6,7 @@ import urllib2
 import xbmcgui
 import xbmcplugin
 
-CHANNELS_URL = 'http://pipes.yahoo.com/pipes/pipe.run?_id=658c65eac05da916cff8929d5f9d8703&_render=json'
+CHANNELS_URL = 'http://chromecastbg.alexmeub.com/images.v4.json'
 
 def showPictures():
     u = urllib2.urlopen(CHANNELS_URL)
@@ -14,13 +14,17 @@ def showPictures():
     u.close()
 
     result = simplejson.loads(data)
-    pictures = result['value']['items']
+    pictures = result
 
     for picture in pictures:
-        
-        item = xbmcgui.ListItem(picture['description'], iconImage = "DefaultPicture.png")
 
-        xbmcplugin.addDirectoryItem(HANDLE, picture['media:group']['media:content']['url'], item)
+        item = xbmcgui.ListItem(picture['name'], iconImage = 'http://chromecastbg.alexmeub.com/images/240_' + picture['name'])
+
+        itm.setInfo('pictures', {
+            'title': 'By ' + picture['photographer']
+        });
+
+        xbmcplugin.addDirectoryItem(HANDLE, 'http://chromecastbg.alexmeub.com/images/1080_' + picture['name'], item)
 
     xbmcplugin.endOfDirectory(HANDLE)
 
